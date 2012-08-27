26f7c9b1ab982e8f02349fb2e87ed602e492aaab
Blaine Bublitz <phate666x@gmail.com>
2012-08-26 19:49:44 -0700

    update to single ticks for consistency

--- example/templates/no_amd/test2.js
+++ example/templates/no_amd/test2.js
@@ -1,5 +1,5 @@
 jade.templates = jade.templates || {};
-jade.templates["test2"] = (function(){
+jade.templates['test2'] = (function(){
   return function anonymous(locals, attrs, escape, rethrow) {
 var attrs = jade.attrs, escape = jade.escape, rethrow = jade.rethrow;
 var __jade = [{ lineno: 1, filename: undefined }];
