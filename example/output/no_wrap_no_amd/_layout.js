61c72a47ae905394774d8251923c15ed800a0b76
azproduction <azazel.private@gmail.com>
2013-03-26 14:30:11 +0600

    ignore example/output

--- example/output/no_wrap_no_amd/_layout.js
+++
@@ -1,9 +0,0 @@
-function anonymous(locals, attrs, escape, rethrow, merge) {
-attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
-var buf = [];
-with (locals || {}) {
-var interp;
-buf.push('<!DOCTYPE html><html lang="en"><head></head><body><div id="container"></div></body></html>');
-}
-return buf.join("");
-}
